6147cf9768b08d15f2cb2938c0bf3481f278dd32
VIKAS VAKIL VISHWAKARMA <vishwakarmavikas0102@gmail.com>
2025-01-25 15:21:42 +0000

    complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,6 +48,39 @@ const scrollup = () =>{
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+   // reset: true, // Animations repeat
+})
+
+sr.reveal(`.home__data, .footer`)
+sr.reveal(`.home__dish`, {delay: 500, distance: '100px', origin: 'bottom'})
+sr.reveal(`.home__burger`, {delay: 1200, distance: '100px', duration: 1500})
+sr.reveal(`.home__ingredient`, {delay: 1600, interval: 100})
+sr.reveal(`.recipe__img, .delivery__img .contact__image`, {origin: 'left'})
+sr.reveal(`.recipe__data, .delivery__data .contact__data`, {origin: 'right'})
+sr.reveal(`.popular__card`, {interval: 100})
